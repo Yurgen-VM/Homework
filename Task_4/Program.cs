f85a52b9ec846c97ff22ba962a679024365f6631
Yuriy <yuriy_vm@inbox.ru>
2024-02-10 00:49:43 +0300

    Determined the digit of the number

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -6,3 +6,20 @@
 Пример: N = 485
 На выходе: 4,8,5
 */
+
+Console.Write("Введите любое натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    int i = 1;
+    while (number /10 > 0)
+    {
+        number = number / 10;
+        i++;
+    } 
+    Console.WriteLine($"{i}");
+} 
+else
+{
+    Console.WriteLine($"{number} не является натруальным числом. Введите другое значение!");
+}
