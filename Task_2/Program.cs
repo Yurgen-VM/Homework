6f1ee2a4703ebb3ab20450564aaaa8160990ea42
Yuriy <yuriy_vm@inbox.ru>
2024-02-09 23:30:30 +0300

    Completed Task_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -5,4 +5,42 @@
 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
 */
 
-
+int sector_1 = 1;
+int sector_2 = 2;
+int sector_3 = 3;
+int sector_4 = 4;
+ 
+Console.Write("Введите значение x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x == 0)
+{ 
+    Console.WriteLine ("x - не может быть равено нулю. Введите другое значение");
+}
+else
+{
+    Console.Write("Введите значение y: ");
+    int y = Convert.ToInt32(Console.ReadLine());
+    if (y == 0) 
+    {
+        Console.WriteLine("y - не может быть равено нулю. Введите другое значение");
+    }
+    else
+    {
+        if (x > 0 && y > 0)
+        {
+             Console.WriteLine($"Точка находится четверти координат № {sector_1}");
+        }
+        if (x < 0 && y > 0)
+        {
+            Console.WriteLine($"Точка находится четверти координат № {sector_2}");
+        }
+        if (x < 0 && y < 0)
+        {
+            Console.WriteLine($"Точка находится четверти координат № {sector_3}");
+        }
+        if (x > 0 && y < 0)
+        {
+            Console.WriteLine($"Точка находится четверти координат № {sector_4}");
+        }
+    }
+}
