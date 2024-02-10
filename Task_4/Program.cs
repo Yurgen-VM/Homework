6dabdb5bab797c6721e2837e307166c9680691a8
Yuriy <yuriy_vm@inbox.ru>
2024-02-10 14:56:36 +0300

    Completed Task_4

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -7,19 +7,53 @@
 На выходе: 4,8,5
 */
 
+using System.Globalization;
+
 Console.Write("Введите любое натуральное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number > 0)
+int number_temp = number;
+int amauntDigit = 1;
+
+if (number_temp > 9)
 {
-    int i = 1;
-    while (number /10 > 0)
+    for (amauntDigit = 1; number_temp / 10 > 0; amauntDigit++)
     {
-        number = number / 10;
-        i++;
+        number_temp = number_temp / 10;
+        amauntDigit = amauntDigit++;
     } 
-    Console.WriteLine($"{i}");
+    //Console.WriteLine($"{amauntDigit}, {number}");
+
+
+    int divisor = 10;
+    for (divisor = 10; amauntDigit > 2; amauntDigit--)
+    {
+        divisor = divisor * 10;
+    } 
+    //Console.WriteLine($"делитель равен: {divisor}");
+   
+   
+    int digit = 1;
+    while (divisor > 1)
+    {
+        digit = number / divisor;
+        divisor/= 10;
+        if (digit >= 10)
+        {
+            digit %= 10; 
+        }
+        Console.Write($"{digit},");    
+    }
+    Console.Write($"{number % 10}");  
 } 
 else
 {
-    Console.WriteLine($"{number} не является натруальным числом. Введите другое значение!");
+    if (number_temp >= 0)
+    {
+        Console.WriteLine($"{number}");
+    }
+    else
+    {
+        Console.WriteLine($"{number} не является натруальным числом. Введите другое значение!");
+    }
+    
 }
